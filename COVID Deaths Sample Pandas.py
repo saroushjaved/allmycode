baeee69c0cef89c702f456508bcbbb2b6d1b145d
soroush <saroushjaved@gmail.com>
2020-06-08 21:11:04 +0500

    Regular Update with new code

---
+++ COVID Deaths Sample Pandas.py
@@ -0,0 +1,63 @@
+# Sample COVID Analysis with Pandas
+# Level : Beginner 
+
+
+import pandas as pd
+
+ 
+df = pd.read_csv(r'/home/soroush/Downloads/owid-covid-data.csv')
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+# Start by Looking for the colums in the data set
+
+##print(df.columns)
+
+# This gives the following output
+##Index(['iso_code', 'continent', 'location', 'date', 'total_cases', 'new_cases',
+##       'total_deaths', 'new_deaths', 'total_cases_per_million',
+##       'new_cases_per_million', 'total_deaths_per_million',
+##       'new_deaths_per_million', 'total_tests', 'new_tests',
+##       'total_tests_per_thousand', 'new_tests_per_thousand',
+##       'new_tests_smoothed', 'new_tests_smoothed_per_thousand', 'tests_units',
+##       'stringency_index', 'population', 'population_density', 'median_age',
+##       'aged_65_older', 'aged_70_older', 'gdp_per_capita', 'extreme_poverty',
+##       'cvd_death_rate', 'diabetes_prevalence', 'female_smokers',
+##       'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand'],
+##      dtype='object') 
+
+
+# So There are multiple copies of same countries and we can now remove them
+
+# df.drop_duplicates(subset="iso_code", keep ="first", inplace = True )
+
+# The Above Function has Dropped the duplicates of countries from your data
+
+# Now Lets make a copy with only the data we need
+
+##copy_df = df.iloc[100:200, 1:12]
+##print(copy_df)
+
+# So from above we can see that the latest date avaliabel to us is 2020-06-08
+# So now lets only have rows that have our latest date
+
+
+copy_date_latest= df[df['date'].str.contains("2020-06-08")]
+
+#print(copy_date_latest.iloc[1:100, 1:12])
+
+# The Above thing is called masking data. The data is masked and only has the for which for
+# Conditional provided at end turns out to eb true
+
+# Now making a final copy of list with only out needed data
+
+copy_final = copy_date_latest.iloc[1:1000, [2,4,6]]
+copy_final.reset_index(drop = True, inplace = True)
+print(copy_final)
+
+
+#Saving the file
+
+copy_final.to_csv(r'/home/soroush/Downloads/covid-data.csv')
---
+++ House Calculator.py
@@ -0,0 +1,26 @@
+# This program is aimed to help you calculate the number of months you need
+# to save for your house
+# Basic Stuff
+
+total_cost = float(input(" Enter the worth of your dream house "))
+annual_salary = float(input( " Enter you annual salary : "))
+down_payment = float(input(" Enter percentage of down payment (0-1) : " ) )
+semi_annual_raise = float(input( " Enter your semi annual raise (0-1) : " ))
+current_savings = 0
+saving_percentage = float(input("Enter your saving percentage"))
+i_rate= 0.04
+time = 0.0
+
+
+while current_savings <= (total_cost* down_payment):
+    
+    monthly_salary = annual_salary/12
+
+    current_savings = current_savings + ( ( monthly_salary * saving_percentage) + (( current_savings*i_rate)/12))
+    time = time + 1
+
+    if (time % 6) == 0:
+        annual_salary = annual_salary * ( 1 + semi_annual_raise )
+    
+        
+print(time)
